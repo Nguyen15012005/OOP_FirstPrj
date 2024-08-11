c8d9ed585634f1332411149062a1c4b23688b500
unknown <23640731.nguyen@student.iuh.edu.vn>
2024-08-11 12:44:18 +0700

    Cập nhật tính năng X và sửa lỗi Y

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Welcome {
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Nam Trung Nguyen!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
 }
